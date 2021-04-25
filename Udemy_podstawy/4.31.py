e14e6a8b42ecaabfeead4f72135820626ce9cfab
Tomasz Szymanski <t_szymanski@onet.eu>
2021-04-25 23:48:35 +0200

    chapter 4.31b

--- Udemy_podstawy/4.31.py
+++ Udemy_podstawy/4.31.py
@@ -1,3 +1,17 @@
 if __name__ == '__main__':
     print('FR')
     print('FR','USA')
+    France = 'FR'
+    print(France, 'USA')
+    pi=3.14
+    r=10
+    print(pi,r)
+    print(pi,r,'P= ',pi*r*r)
+    print('pole kola o promieniu', r, '= ', pi * r **2)
+    print(1,2,3)
+    print(1,2,3,sep='-')
+    print(1,2,3,sep='\n')
+    print(1, 2, 3, sep='\t')
+    print('this is bell: \a')
+    print('\u03A3')
+    print('this is backslash: \\')
