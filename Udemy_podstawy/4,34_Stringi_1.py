0838c975fec9dfc6e66750ded9fd099ef03075d8
Tomasz Szymanski <t_szymanski@onet.eu>
2021-04-26 08:20:34 +0200

    4.34 String

---
+++ Udemy_podstawy/4,34_Stringi_1.py
@@ -0,0 +1,35 @@
+if __name__ == '__main__':
+    atext="this is a text."
+    print(atext)
+
+    print(atext.endswith('etx'))
+
+    print(atext.islower())
+
+    print(atext.upper())
+
+    print(atext.islower())
+
+    print(atext.find('is'))
+
+    print(atext.find('is'))
+
+    print(atext.find('is',3))
+
+    print(atext.replace('a','4'))
+
+    print(atext.replace('a', '4').replace('i','1').replace('e','3'))
+
+    print(atext.split(' '))
+
+    somethingLikeNumber='1000'
+
+    #print(somethingLikeNumber+1)
+
+    print(somethingLikeNumber.isdigit())
+
+    print(somethingLikeNumber.isdecimal())
+
+    print(somethingLikeNumber.isalpha())
+
+    print(somethingLikeNumber.isalnum())
