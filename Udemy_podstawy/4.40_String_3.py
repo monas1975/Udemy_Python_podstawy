3ab9f08a63d4acdc6029bd41d1282c32dca35d7e
t_szymanski@onet.eu <t_szymanski@onet.eu>
2021-04-27 06:45:42 +0200

    4.40

---
+++ Udemy_podstawy/4.40_String_3.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    somethingLikeNumber= '1000'
+    print(int(somethingLikeNumber)+1)
+
+
+    somethingLikeNumber2 = '1000'
+    a = somethingLikeNumber2+ str(1)
+    print(a)
+
