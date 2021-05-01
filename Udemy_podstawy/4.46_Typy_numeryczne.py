c4a9352476cd3fd5d92f1880a00fde3346c42de4
t_szymanski@onet.eu <t_szymanski@onet.eu>
2021-05-01 09:28:11 +0200

    4.46

---
+++ Udemy_podstawy/4.46_Typy_numeryczne.py
@@ -0,0 +1,34 @@
+if __name__ == '__main__':
+ five = 5
+ three = 3
+ print(five*three)
+ print(type(five*three))
+
+ print(five/three)
+ print(type(five/three))
+
+ import sys
+ print(sys.maxsize)
+
+ print(sys.version)
+
+ veryBigValue1 = 9999999999999999999999999999999999
+ print(veryBigValue1)
+
+ veryBigValue2 = 99999999999999999999999999999999999999999999999999999999
+ print(veryBigValue2)
+ print(veryBigValue2+1)
+ print(type(veryBigValue2))
+ print((veryBigValue2+1)/2)
+ print(type((veryBigValue2+1)/2))
+
+ print(five//three)
+ print(five%three)
+
+ print(float('inf')) #infinity - nieskonczonosc
+
+ print(float('inf')>999999999999999)
+
+ print(-float('inf'))
+
+
