5aaf2adb2abd8bbd953ce56d1677419b5af9e699
Tomasz Szymanski <t_szymanski@onet.eu>
2021-05-04 21:48:21 +0200

    4.53

---
+++ Udemy_podstawy/4_52_Zmienne_Tips_and_Tricks.py
@@ -0,0 +1,36 @@
+if __name__ == '__main__':
+    Title = 'Pythone'
+    print('Title is ', type(Title))
+
+    Version = 3
+    print('Version is', type(Version))
+
+    Progress = 0.21
+    print('Progress =', type(Progress))
+
+    print(type(Version*Progress))
+
+    x=1
+    y=1
+    z=1
+    print(x,y,z)
+
+    a=b=c=2
+    print(a,b,c)
+    c=3
+    print(a, b, c)
+
+    print(6/2*(1+2))
+
+    print(4**3**2)
+    print(4**6)
+    print(4**3**2 == 4**6)
+    x=1
+    print(x+1)
+
+    x=x+1
+    print(x)
+    x+=1
+    print(x)
+    x-=1
+    print(x)
---
+++ Udemy_podstawy/4_53_Zmienne_Tips_and_Trick_LAB.py
@@ -0,0 +1,40 @@
+if __name__ == '__main__':
+    #1. Zadeklaruj zmienne:
+#v1 o wartości 126
+#v2 o wartości '126'
+#v3 o wartości 126.0
+#v4 o wartości '126.0'
+
+  v1 = 126
+  v2 = '126'
+  v3 = 126.0
+  v4 = '126.0'
+
+#2. Zastanów się
+# (na razie bez sprawdzania), jaki typ mają te zmienne
+
+# 3. Poleceniami print wyświetl wartości zmiennych
+# i ich typ. Dobrze odgadłeś(aś) typ zmiennych?
+  print(v1,type(v1))
+  print(v2, type(v2))
+  print(v3, type(v3))
+  print(v4, type(v4))
+
+#4. Poleceniami print wyświet:
+#wynik dodawania v1 i v3 oraz typ tak wyznaczonego wyniku
+#wynik dodawania v2 i v4 oraz typ tak wyznaczonego wyniku
+  print(v1+v3,type(v1+v3))
+  print(v2 + v4, type(v2 + v4))
+
+#5. Ile to jest:
+#7-1*0+3+3
+#Najpierw policz w pamięci, a potem z Pythonem
+  print(7-1*0+3+3)
+
+#6. Ile to jest  4 do potęgi piątej podzielone
+# przez 2 do potęgi 3
+  print(4**5/2**3)
+  print(99+(9/9))
+
+
+
