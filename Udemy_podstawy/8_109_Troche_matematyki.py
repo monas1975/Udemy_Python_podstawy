36dae0c2588dc8e9b356ac00d9b5c4392f8398a6
Tomasz Szymanski <t_szymanski@onet.eu>
2021-05-09 01:30:02 +0200

    8_110

---
+++ Udemy_podstawy/8_109_Troche_matematyki.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    f_smaller = 3.141592653589793238462643383279
+    f_bigger= 3.87756392764
+
+    print(f_smaller, f_bigger)
+    print("\n")
+
+    print(int(f_smaller),int(f_bigger))
+    print("\n")
+
+    print((abs(f_smaller),abs (f_bigger)))
+    print("\n")
+
+    #3
+    print('+++++++++++++++++++++++++++++')
+    print(round(f_smaller,2), round(f_bigger,2))
+    print("\n")
+    print('+++++++++++++++++++++++++++++')
+    list=[1,2,3,4,5,4,4,5,4]
+    print(sum(list), len(list))
+    print("\n")
+
+    print(sum(list) / len(list))
+    print("\n")
+
+    print(round(2.675,2))
